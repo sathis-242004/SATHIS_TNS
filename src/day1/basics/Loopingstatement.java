a129d0880ddc1a5de167cc77ac4682c308a80d11
Saqilin <saqlinmustaq17@gmail.com>
2025-08-06 14:48:34 +0530

    COMMIT 1

---
+++ src/day1/basics/Loopingstatement.java
@@ -0,0 +1,40 @@
+package day1.basics;
+
+public class Loopingstatement {
+
+	public static void main(String[] args) {
+		
+		byte a = 100;
+		byte b= -101;
+		
+		System.out.println("b range of byte is" +
+				b+"a range of byte is "+a);
+		
+		short C= 32;
+		short D= -36;
+		System.out.println("D range of byte is" +
+				D+"C range of byte is "+C);
+		
+		//int - 4bytes
+		int E = 47;
+		int F= -48;
+		System.out.println("F range of byte is" +
+				F+"E range of byte is "+E);
+		
+		//long - 8bytes
+		long G= 7L;
+		long H = -9L;
+		
+		System.out.println("H range of byte is" +
+				H+"G range of byte is "+G);
+		
+		float f=24.243278345f;
+		double d=46.4512345678902345678914f;
+		System.out.println("float value is "+f+" double value is "+d);
+		
+		//boolean 
+		boolean flag=false;
+		System.out.println("boolean value is "+flag);
+	}
+}
+
---
+++ src/day1/basics/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+package day1.basics;
